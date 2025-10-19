986a4b6f775d95f978398953f4c142246afad548
xan_d_or <alexrommak@gmail.com>
2025-10-19 20:29:29 +0300

    Дисбаланс по брендам

--- utils/eda_utils.py
+++ utils/eda_utils.py
@@ -1,6 +1,7 @@
+import numpy as np
 import os
 import pandas as pd
-
+from rapidfuzz import fuzz
 
 def directory_to_dataframe(path_to_dataset: str):
 
@@ -29,3 +30,25 @@ def directory_to_dataframe(path_to_dataset: str):
     # Создаём DataFrame
     df = pd.DataFrame(data)
     return df
+
+def sneaker_class_to_brand(unique_classes: np.ndarray
+                           ) -> dict[str, str]:
+    BRANDS = ['adidas', 'asics', 'converse', 'new_balance',
+              'nike', 'puma', 'reebok', 'salomon', 'vans', 'yeezy']
+
+    # Порог похожести (можно варьировать, 70–85 обычно ок)
+    SIMILARITY_THRESHOLD = 95
+
+    def find_brand(sneaker_class):
+        for brand in BRANDS:
+            score = fuzz.partial_ratio(sneaker_class, brand)
+            if score > SIMILARITY_THRESHOLD:
+                return brand
+        # Если не найдено — добавляем как новый "бренд"
+        return sneaker_class
+
+    # Определяем базовый бренд для каждого класса
+    class_to_brand = {}
+    for c in unique_classes:
+        class_to_brand[c] = find_brand(c)
+    return class_to_brand
