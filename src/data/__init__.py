12b870ad64727eea949eedfff74e4033b504b0b2
Azat352 <ahm352@gmail.com>
2026-04-06 22:24:35 +0300

    fix init in src.data

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,5 @@
 from .dataset import LitDataImageDataset
-from .label2idx import label2idx
+from .label2idx import build_label2idx_s3
 from .s3_client import YandexS3Client
 
-__all__ = ["LitDataImageDataset", "label2idx", "YandexS3Client"]
+__all__ = ["LitDataImageDataset", "build_label2idx_s3", "YandexS3Client"]
